c5dadc54c3769798b78647584b27692a6b29c87b
Hans Ole Gjerdrum <hansogj@gmail.com>
2023-08-19 10:24:27 +0200

    fix(build): [] common webpack config

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,15 +1,14 @@
-
 const path = require("path");
 const webpack = require("../../webpack.build.js");
 const config = webpack();
 
 module.exports = () => ({
     ...config,
-    entry: "./src/abonnement.ts",
+    entry: "./src/index.ts",
     output: {
         ...config.output,
         path: path.resolve(__dirname, "dist"),
-        filename: 'abonnement.js',
-        library: 'abonnement',
+        filename: 'index.js',
+        library: '[package-name]',
     }
 });
