18ce673d377a97331bbeed5f027bb85ece89bc27
Hans Ole Gjerdrum <hansogj@gmail.com>
2024-04-21 10:12:11 +0200

    npm bump

--- packages/immer-reduxer/src/utils.ts
+++ packages/immer-reduxer/src/utils.ts
@@ -1,19 +1,22 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
-import { ActionDataType } from './types';
+import {ActionDataType, Data} from './types';
 
 export const selectFromRoot = <State, Key extends keyof State>(state: State, select: Key): State[Key] =>
     state && select && state[select];
 
-export const toAction = <T, P>(type: T, data: Partial<P> = {}): ActionDataType<T, P> => ({
+export const toAction = <T extends string = string, P extends Data = Data>(
+    type: T,
+    data: Partial<P> = {}
+): ActionDataType<T, P> => ({
     ...data,
     type,
 });
 
-export const fromActionType = <Actions, ActionData>(type: Actions) => ({
+export const fromActionType = <Actions extends string = string, ActionData extends string = string>(type: Actions) => ({
     withData:
         <
-            ActionType extends Pick<Partial<ActionData>, keyof ActionData> & { action: Actions },
+            ActionType extends Pick<Partial<ActionData>, keyof ActionData> & {action: Actions},
             Key extends keyof ActionData,
         >(
             ...fields: Array<Key>
@@ -21,6 +24,6 @@ export const fromActionType = <Actions, ActionData>(type: Actions) => ({
         (...data: Array<ActionData[Array<Key>[number]]>) =>
             toAction<Actions, ActionType>(
                 type,
-                (fields || []).reduce((aggregate, key, index) => ({ ...aggregate, [key]: data[index] }), {}),
+                (fields || []).reduce((aggregate, key, index) => ({...aggregate, [key]: data[index]}), {})
             ),
 });
