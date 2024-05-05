1d216bfc7932f64d3f7093d43f2bd6aa439edd3c
Hans Ole Gjerdrum <hansogj@gmail.com>
2024-05-05 11:04:50 +0200

    npm bump (#12)
    * npm bump
    
    * node engines

--- packages/immer-reduxer/src/types.ts
+++ packages/immer-reduxer/src/types.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 
-import { Draft } from 'immer';
+import {Draft} from 'immer';
 
 export type ReduxAction = import('redux').Action;
 
-interface Action<T> extends ReduxAction {
+export type Data = Record<string, unknown>;
+
+interface Action<T extends string = string> extends ReduxAction {
     type: T;
 }
 
@@ -12,9 +14,14 @@ export type Mutable<Type> = {
     -readonly [Key in keyof Type]: Type[Key];
 };
 
-export type ActionDataType<T, P> = Action<T> & Partial<P>;
-export type ActionTypesValues<T, P> = ActionDataType<T, P>[keyof ActionDataType<T, P>];
-export type DispatchAction<T, P> = (..._: ActionTypesValues<T, P>[]) => ActionDataType<T, P>;
+export type ActionDataType<T extends string = string, P extends Data = Data> = Action<T> & Partial<P>;
+export type ActionTypesValues<T extends string = string, P extends Data = Data> = ActionDataType<
+    T,
+    P
+>[keyof ActionDataType<T, P>];
+export type DispatchAction<T extends string = string, P extends Data = Data> = (
+    ..._: ActionTypesValues<T, P>[]
+) => ActionDataType<T, P>;
 
 export type Producer<State, Action> = (draft: Draft<State>, action: Action) => void;
 export type SharedKeys<Type, OtherType> = keyof Type & keyof OtherType;
